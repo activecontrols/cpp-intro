537c7e622699516d3e648c3f2e426034ab0c285f
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2022-02-10 18:03:37 -0500

    Add main boilerplate

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Needed for CLI I/O
+#include <iostream>
+
+// Namespaces - learn about them later.
+// Basically allows us to access things like std::string without having
+// to specify the scope (i.e. string my_str;)
+using namespace std;
+
+// Main function; this is where our execution will start.
+int main(int argc, char *argv[]) {
+    // Your code goes here:
+}
